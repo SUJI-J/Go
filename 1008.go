5e672760b78263b26a1ae2a64f9f619b0f28c172
SUJI-J <80563308+SUJI-J@users.noreply.github.com>
2022-02-09 13:22:09 +0900

    Update 1008.go

--- 1008.go
+++ 1008.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var num1 int
+	var num2 int
+
+	fmt.Scanln(num1, num2)
+	var result float32 = (float32)(num1 / num2)
+	fmt.Printf("%f", result)
 }
