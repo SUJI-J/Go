af3797200bb297e3587451d2b95a45fe12344310
SUJI-J <80563308+SUJI-J@users.noreply.github.com>
2022-02-09 13:25:01 +0900

    Update 2741.go

--- 2741.go
+++ 2741.go
@@ -1,5 +1,16 @@
-package main
+package main //시간초과
+
+import (
+	"fmt"
+)
 
 func main() {
+	var row int
 
+	fmt.Scanf("%d\n", &row)
+
+	for i := 1; i <= row; i++ {
+		fmt.Printf("%d\n", i)
+
+	}
 }
