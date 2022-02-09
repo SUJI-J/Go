9443a436eb948be379b76d4ec1e98cbcfa2a8687
SUJI-J <80563308+SUJI-J@users.noreply.github.com>
2022-02-09 13:23:27 +0900

    Update 1330.go

--- 1330.go
+++ 1330.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var num1 int
+	var num2 int
+
+	fmt.Scanln(&num1, &num2)
+
+	if num1 > num2 {
+		fmt.Println(">")
+	} else if num1 < num2 {
+		fmt.Println("<")
+	} else if num1 == num2 {
+		fmt.Println("==")
+	}
 }
