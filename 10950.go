41eacc4de9c61c629c11a6958c204309130d4fc1
SUJI-J <80563308+SUJI-J@users.noreply.github.com>
2022-02-09 13:23:08 +0900

    Update 10950.go

--- 10950.go
+++ 10950.go
@@ -1,5 +1,16 @@
-package main
+package main //배열 사용해야돼?
+
+import "fmt"
 
 func main() {
+	var num1 int
+	var num2 int
+	var row int
 
+	fmt.Scanln(&row)
+
+	for i := 1; i <= row; i++ {
+		fmt.Scan(&num1, &num2)
+		fmt.Println(num1 + num2)
+	}
 }
