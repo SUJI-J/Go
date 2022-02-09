c3f9ff27fb75f822eb3e9709fcca749d19113502
SUJI-J <80563308+SUJI-J@users.noreply.github.com>
2022-02-09 13:25:41 +0900

    Update 9498.go

--- 9498.go
+++ 9498.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var score int
+
+	fmt.Scanln(&score)
+
+	if score >= 90 {
+		fmt.Println("A")
+	} else if score >= 80 {
+		fmt.Println("B")
+	} else if score >= 70 {
+		fmt.Println("C")
+	} else if score >= 60 {
+		fmt.Println("D")
+	} else {
+		fmt.Println("F")
+	}
 }
