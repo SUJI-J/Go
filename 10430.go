35f51a4da24dd6fe68f029c14fad9753d800f98f
SUJI-J <80563308+SUJI-J@users.noreply.github.com>
2022-02-09 13:22:26 +0900

    Update 10430.go

--- 10430.go
+++ 10430.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var num1 int
+	var num2 int
+	var num3 int
+
+	fmt.Scanf("%d %d %d", &num1, &num2, &num3)
+
+	fmt.Println((num1 + num2) % num3)
+	fmt.Println(((num1 % num3) + (num2 % num3)) % num3)
+
+	fmt.Println((num1 * num2) % num3)
+	fmt.Println(((num1 % num3) * (num2 % num3)) % num3)
 }
