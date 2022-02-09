4812903b4b6d220b0bf252ce4e1edd9864c505dc
SUJI-J <80563308+SUJI-J@users.noreply.github.com>
2022-02-09 13:23:42 +0900

    Update 14681.go

--- 14681.go
+++ 14681.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var x int
+	var y int
+
+	fmt.Scanln(&x)
+	fmt.Scanln(&y)
+
+	if x > 0 && y > 0 {
+		fmt.Println("1")
+	} else if x < 0 && y > 0 {
+		fmt.Println("2")
+	} else if x < 0 && y < 0 {
+		fmt.Println("3")
+	} else if x > 0 && y < 0 {
+		fmt.Println("4")
+	}
 }
