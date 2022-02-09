4144772894c55aca6485b66f0d0800296ce1a9d8
SUJI-J <80563308+SUJI-J@users.noreply.github.com>
2022-02-09 13:23:58 +0900

    Update 2438.go

--- 2438.go
+++ 2438.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var row int
+
+	fmt.Scanf("%d", &row)
+
+	for i := 1; i <= row; i++ {
+		for j := 0; j < i; j++ {
+			fmt.Print("*")
+		}
+		fmt.Print("\n")
+	}
 }
